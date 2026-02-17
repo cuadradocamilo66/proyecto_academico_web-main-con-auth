00332342ac9ba91fb489d4be6b31b590f06edaab
cuadradocamilo66 <cuadradocamilo66@gmail.com>
2026-02-16 21:52:26 -0500

    nuevo

--- app/observaciones/page.tsx
+++ app/observaciones/page.tsx
@@ -1,11 +1,6 @@
-
+"use client"
 import { AppShell } from "@/components/layout/app-shell"
 import { ObservationsView } from "@/components/observations/observations-view"
-
-export const metadata = {
-  title: "Observaciones | Plataforma Académica",
-  description: "Gestión de observaciones de estudiantes",
-}
 
 export default function ObservacionesPage() {
   return (
